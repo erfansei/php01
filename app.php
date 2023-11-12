56db4fc90fa903273995af04894b5461bfcd0aea
erfansei <erfan.saleki@gmail.com>
2023-11-12 21:03:14 +0330

    php-s01

---
+++ app.php
@@ -0,0 +1,20 @@
+<title>
+    <?php
+        echo "ErfanSaleki-PHP-Session1"
+    ?>
+</title>
+<h1>
+    <?php
+        echo "PHP Description";   
+    ?>
+</h1>
+<img src="
+    <?php
+        echo "https://www.php.net/images/meta-image.png";
+    ?>
+" title="php-logo"/>
+<p>
+    <?php
+        echo "PHP (Hypertext Processor) is a general-purpose scripting language and interpreter that is freely available and widely used for web development. The language is used primarily for server-side scripting, although it can also be used for command-line scripting and, to a limited degree, desktop applications.";
+    ?>
+</p>
